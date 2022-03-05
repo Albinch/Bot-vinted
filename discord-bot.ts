653b41c7972574f5e9ccbfc31fa2de1e3acda540
Clément Alba <clement.alba2.etu@univ-lille.fr>
2022-03-05 17:35:55 +0100

    First fonctionnal version !

--- discord-bot.ts
+++ discord-bot.ts
@@ -1,7 +1,9 @@
 import * as Discord from "discord.js";
 import * as fs from "fs";
+import { watch } from "./vinted-bot";
+import Item from "./item";
 
-const client = new Discord.Client({ intents : [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]});
+export const client = new Discord.Client({ intents : [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]});
 const bot = JSON.parse(fs.readFileSync("./discord.json", "utf-8")).bot;
 
 client.on("ready", () => {
@@ -9,11 +11,20 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", async function(message){
+    let prefix = "!";
     if(message.channel.type !== "DM"){
-        if(message.content === "salope"){
-            message.reply("loli");
+        if(message.content === prefix + "start"){
+            let items: Array<Item> = [];
+            await watch(items, message.channel);
         }
     }
+});
+
+client.on("newItemsEvent", async function sendAlert(item: Item, channel: Discord.AnyChannel){
+    (channel as Discord.TextChannel).send("Hey <@everyone>, j'ai trouvé un nouvel article !<:star_struck:949371085181296711>\n"
+                                        + "- Prix : " + item.price + " " + item.currency + "\n"
+                                        + "- Taille : " + item.size + "\n"
+                                        + "Cours le cliquer ! <:arrow_right:949371810296778843>\n" + item.url + String(item.id));
 });
 
 client.login(bot.bot_token);
--- vinted-bot.ts
+++ vinted-bot.ts
@@ -1,6 +1,8 @@
 import * as vinted from "vinted-api";
+import * as Discord from "discord.js";
 import * as fs from "fs";
 import Item from "./item";
+import { client } from "./discord-bot";
 
 async function storeItems(items: Array<Item>){
     const brands = JSON.parse(fs.readFileSync("./brands.json", "utf-8")).brands;
@@ -17,25 +19,41 @@ async function storeItems(items: Array<Item>){
         sizeParameters += "size_id[]=" + element.id + "&";
     });
 
-    const posts = await vinted.search(baseUrl + brandParameters.slice(0, -1) + sizeParameters.slice(0, -1) + "&order=newest_first");
+    try{
+        const posts = await vinted.search(baseUrl + brandParameters + sizeParameters.slice(0, -1) + "&order=newest_first", true);
 
-    posts.items.forEach((element: any) => {
-        let currentItem = new Item(element.id, Number(element.price), element.currency, element.brand_title, element.url, element.size_title);
-        if(!currentItem.isInsideList(items)){
-            items.push(currentItem);
-        }
-    });
+        posts.items.forEach((element: any) => {
+            let currentItem = new Item(element.id, Number(element.price), element.currency, element.brand_title, element.url, element.size_title);
+            if(!currentItem.isInsideList(items)){
+                items.push(currentItem);
+            }
+        });
+    }catch(e){
+        console.log(e);
+    }
 }
 
-async function watch(items: Array<Item>){
-    let nbItems = items.length;
+export async function watch(items: Array<Item>, channel: Discord.AnyChannel){
+    const bot: any = JSON.parse(fs.readFileSync("./discord.json", "utf-8")).bot;
+
+    client.login(bot.bot_token);
+    
+    let nbItems: number = items.length;
     console.log("initialisation : " + nbItems);
     while(true){
-        await delay(1000);
+        await delay(5000);
         storeItems(items);
         if(items.length != nbItems){
+            let itemsPublished: number = items.length - nbItems;
+            
+            if(itemsPublished < 5){
+                for(let i = 0; i < itemsPublished; i++){
+                    client.emit("newItemsEvent", items[nbItems + i], channel);
+                }
+            }
+
             nbItems = items.length;
-            console.log("nbItems incrémenté : " + nbItems);
+            console.log("nbItems incrémenté : " + nbItems); 
         }
     }
 }
@@ -44,9 +62,10 @@ async function delay(ms: number) {
     return new Promise( resolve => setTimeout(resolve, ms) );
 }
 
+ /*
 async function main(){
     let items: Array<Item> = [];
     await watch(items);
 }
 
-main()
+main()*/
